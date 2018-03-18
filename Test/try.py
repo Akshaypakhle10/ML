74d8a72e815dfb8d80ab52fac4fc5e7d35227e98
AkshayPakhle10 <akshay.pakhle888@gmail.com>
2018-03-18 15:46:44 +0530

    Pre-processing and scaling done.

---
+++ Test/try.py
@@ -0,0 +1,49 @@
+#Importing libraries
+import pandas as pd
+
+#Loading the dataset
+df = pd.read_csv('purchase_data_class_4.csv')
+
+#Reviewing dataset
+df.head()
+
+
+#Separating features and labels
+X = df.iloc[:, :-1].values
+y = df.iloc[:, 3:].values
+print X, y
+
+#Check for missing data
+df.isnull().sum()
+
+#Handling Missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = "mean", axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+print X
+
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0]) #Index of the categorical column
+print X.shape
+X = onehotencoder.fit_transform(X).toarray()
+
+labelencoder_y = LabelEncoder()
+y[:, 0] = labelencoder_y.fit_transform(y[:, 0]) #Trivia : Why are we not using OneHotEncoder?
+print X.shape, y.shape
+
+#Splitting into Train and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 0) #Trivia : What is random_state?
+print 'X Train', X_train, '\n\nX Test', X_test, '\n\nY Train', y_train, '\n\nY Test',y_test
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test) #Trivia : Why only transform?
+print X_train, X_test
+
